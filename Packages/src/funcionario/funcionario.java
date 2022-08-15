73f1718ad735f774922c9279d677d817e622032c
Gustavo Pacheco Filho <78632033+gustavopachecof@users.noreply.github.com>
2022-08-14 22:41:46 -0300

    Add files via upload

---
+++ Packages/src/funcionario/funcionario.java
@@ -0,0 +1,12 @@
+package funcionario;
+
+public class funcionario {
+    public static void ferias(boolean ferias ) {
+        if (ferias == false) {
+            System.out.println("\nEstou trabalhando!");
+        } else {
+            System.out.println("\nFérias!");
+        }
+        ;
+    }
+}
